58294cceff2a499e51d67fb6dc6de9cf2149e346
oborodulin <soft.oborodulin@yandex.ru>
2024-03-31 17:54:38 +0300

    Data: OSM (House) model continue...

--- data_territory/src/main/java/com/oborodulin/jwsuite/data_territory/remote/osm/mappers/house/HouseElementToHouseMapper.kt
+++ data_territory/src/main/java/com/oborodulin/jwsuite/data_territory/remote/osm/mappers/house/HouseElementToHouseMapper.kt
@@ -1,6 +1,5 @@
 package com.oborodulin.jwsuite.data_territory.remote.osm.mappers.house
 
-import com.oborodulin.home.common.extensions.toIntHouseNum
 import com.oborodulin.home.common.mapping.Mapper
 import com.oborodulin.jwsuite.data_geo.remote.osm.mappers.GeometryToGeoCoordinatesMapper
 import com.oborodulin.jwsuite.data_territory.remote.osm.model.house.HouseElement
@@ -9,45 +8,11 @@ import com.oborodulin.jwsuite.domain.model.geo.GeoStreet
 import com.oborodulin.jwsuite.domain.model.territory.House
 import com.oborodulin.jwsuite.domain.types.BuildingType
 
-// https://wiki.openstreetmap.org/wiki/RU:Key:building#%D0%9A%D0%BE%D0%BC%D0%BC%D0%B5%D1%80%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B5
-// https://taginfo.openstreetmap.org/keys/building#values
-// https://wiki.openstreetmap.org/wiki/RU:%D0%90%D0%B4%D1%80%D0%B5%D1%81%D0%B0#%D0%9D%D1%83%D0%BC%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D1%8F_%D0%B4%D0%BE%D0%BC%D0%BE%D0%B2
 class HouseElementToHouseMapper(private val mapper: GeometryToGeoCoordinatesMapper) :
     Mapper<HouseElement, House> {
     override fun map(input: HouseElement): House {
         with(input.tags) {
-            val type = when {
-                listOf(
-                    "bungalow",
-                    "cabin",
-                    "detached",
-                    "farm",
-                    "ger",
-                    "houseboat",
-                    "semidetached_house",
-                    "static_caravan",
-                    "stilt_house",
-                    "terrace",
-                    "tree_house",
-                    "trullo",
-                    "hut"
-                ).contains(this.building) -> BuildingType.HOUSE
-
-                listOf("yes", "residential").contains(this.building) -> BuildingType.HOUSE
-                "commercial" == this.building -> BuildingType.OFFICE
-                "kiosk" == this.building -> BuildingType.RETAIL
-                listOf(
-                    "industrial",
-                    "warehouse",
-                    "shed"
-                ).contains(this.building) -> BuildingType.INDUSTRIAL
-
-                else -> try {
-                    BuildingType.valueOf(this.building.trim().uppercase())
-                } catch (e: IllegalArgumentException) {
-                    BuildingType.PUBLIC
-                }
-            }
+            val type = this.buildingType()
             return House(
                 //val microdistrict: GeoMicrodistrict? = null,
                 street = GeoStreet().also { it.id = this.streetId },
@@ -55,7 +20,7 @@ class HouseElementToHouseMapper(private val mapper: GeometryToGeoCoordinatesMapp
                     GeoLocalityDistrict().also { it.id = ldId }
                 },
                 zipCode = this.postcode.ifBlank { null },
-                houseNum = this.houseNumber.toIntHouseNum(),
+                houseNum = this.houseNum(),
                 houseLetter = House.letterFromHouseFullNum(this.houseNumber),
                 buildingNum = House.buildingNumFromHouseFullNum(this.houseNumber),
                 buildingType = type,
--- data_territory/src/main/java/com/oborodulin/jwsuite/data_territory/remote/osm/model/house/HouseApiModel.kt
+++ data_territory/src/main/java/com/oborodulin/jwsuite/data_territory/remote/osm/model/house/HouseApiModel.kt
@@ -2,6 +2,7 @@ package com.oborodulin.jwsuite.data_territory.remote.osm.model.house
 
 import com.oborodulin.jwsuite.data_geo.remote.osm.model.Geometry
 import com.oborodulin.jwsuite.data_geo.remote.osm.model.Osm3s
+import com.oborodulin.jwsuite.domain.types.BuildingType
 import com.oborodulin.jwsuite.domain.util.Constants.OSM_TIMEOUT
 import com.squareup.moshi.Json
 import java.util.UUID
@@ -43,6 +44,9 @@ data class HouseElement(
     @Json(name = "tags") val tags: HouseTags
 )
 
+// https://wiki.openstreetmap.org/wiki/RU:Key:building#%D0%9A%D0%BE%D0%BC%D0%BC%D0%B5%D1%80%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B5
+// https://taginfo.openstreetmap.org/keys/building#values
+// https://wiki.openstreetmap.org/wiki/RU:%D0%90%D0%B4%D1%80%D0%B5%D1%81%D0%B0#%D0%9D%D1%83%D0%BC%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D1%8F_%D0%B4%D0%BE%D0%BC%D0%BE%D0%B2
 data class HouseTags(
     @Json(name = "osmType") val osmType: String,
     @Json(name = "streetId") val streetId: UUID,
@@ -53,4 +57,53 @@ data class HouseTags(
     @Json(name = "flats") val flats: String,
     @Json(name = "levels") val levels: String,
     @Json(name = "max_level") val maxLevel: String
-)
+) {
+    companion object {
+        const val NUMBER_DELIMITERS = " /-"
+    }
+
+    fun buildingType(): BuildingType = when {
+        listOf(
+            "bungalow",
+            "cabin",
+            "detached",
+            "farm",
+            "ger",
+            "houseboat",
+            "semidetached_house",
+            "static_caravan",
+            "stilt_house",
+            "terrace",
+            "tree_house",
+            "trullo",
+            "hut"
+        ).contains(this.building) -> BuildingType.HOUSE
+
+        listOf("yes", "residential").contains(this.building) -> BuildingType.HOUSE
+        "commercial" == this.building -> BuildingType.OFFICE
+        "kiosk" == this.building -> BuildingType.RETAIL
+        listOf(
+            "industrial",
+            "warehouse",
+            "shed"
+        ).contains(this.building) -> BuildingType.INDUSTRIAL
+
+        else -> try {
+            BuildingType.valueOf(this.building.trim().uppercase())
+        } catch (e: IllegalArgumentException) {
+            BuildingType.PUBLIC
+        }
+    }
+
+    fun houseNum(): Int {
+        var number = ""
+        NUMBER_DELIMITERS.forEach { delimiter ->
+            number =
+                houseNumber.takeIf { it.contains(delimiter) }?.substringBefore(delimiter).orEmpty()
+        }
+        return number.ifEmpty { houseNumber }.filter { it.isDigit() }.toInt()
+    }
+
+    fun houseLetter() = houseNumber.substringBefore(' ').filter { it.isLetter() }.ifEmpty { null }
+    fun buildingNum() = houseNumber.substringAfter(' ').filter { it.isDigit() }.toIntOrNull()
+}
